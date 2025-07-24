5e7761b8423d4d767deb423535a6845248776447
leolai <leolai@sunsky.com.tw>
2025-07-24 15:27:21 +0800

    刪除抽取餐點相關

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -3,9 +3,6 @@ import user from './user';
 import admin from './admin';
 import health from './health';
 import upload from './upload';
-import record from './record';
-import cuisineTypes from './cuisineType';
-import mealPresets from './mealPeriodPreset';
 import places from './places';
 
 const router = Router();
@@ -14,9 +11,6 @@ router.use('/user', user);
 router.use('/admin', admin);
 router.use('/health', health);
 router.use('/upload', upload);
-router.use('/record', record);
-router.use('/cuisineTypes', cuisineTypes);
-router.use('/mealPresets', mealPresets);
 router.use('/places', places);
 
 export default router;
